a7e3ee8fcb743a49fed3ae68f021bf27bc132701
Sebastian <sebastian.gornicki@protonmail.com>
2019-06-11 17:24:48 +0200

    Initial commit (test)

---
+++ src/main/java/tutorial/basic/PrimitiveTypes.java
@@ -0,0 +1,21 @@
+package tutorial.basic;
+
+public class PrimitiveTypes {
+
+    byte byteDefault;
+
+    short shortDefault;
+
+    int intDefault;
+
+    long longDefault;
+
+    float floatDefault;
+
+    double doubleDefault;
+
+    char charDefault;
+
+    boolean booleanDefault;
+
+}
---
+++ src/main/java/tutorial/basic/Variables.java
@@ -0,0 +1,29 @@
+package tutorial.basic;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte myByteMax = 127;
+        byte myByteMin = -128;
+
+        short myShortMax = 32767;
+        short myShortMin = -32768;
+
+        int myIntMax = 2147483647;
+        int myIntMin = -2147483648;
+
+        long myLongMax = 9223372036854775807l; //ponad dziewięć trylionów
+        long myLongMin = -9223372036854775808l;
+
+        float myFloat = 9.9999999f ; //max ok 6-7 liczb po przecinku
+
+        double myDouble = 9.9999999999; // max ok 15 cyfr po przecinku
+
+        char myChar = 'x';
+
+        boolean myBooleanTrue = true;
+        boolean myBooleanFalse = false;
+
+    }
+}
