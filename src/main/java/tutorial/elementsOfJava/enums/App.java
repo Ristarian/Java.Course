6e3f18902bf6e3abf488eec61415df5e401952f5
Sebastian <sebastian.gornicki@protonmail.com>
2019-06-22 17:34:53 +0200

    New commit - 22/06/2019

--- src/main/java/tutorial/elementsOfJava/enums/App.java
+++ src/main/java/tutorial/elementsOfJava/enums/App.java
@@ -1,4 +1,36 @@
 package tutorial.elementsOfJava.enums;
 
 public class App {
+
+    public static void main(String[] args) {
+
+        Animal animal = Animal.CAT;
+
+        switch (animal){
+            case CAT:
+                System.out.println("cat");
+                break;
+            case DOG:
+                System.out.println("dog");
+                break;
+            case MOUSE:
+                System.out.println("mouse");
+                break;
+            default:
+                break;
+        }
+
+        System.out.println(Animal.CAT);
+
+        System.out.println(Animal.DOG.getClass());
+
+        System.out.println(Animal.DOG instanceof Enum);
+
+        System.out.println(Animal.MOUSE.getName());
+
+        Animal animal2 = Animal.valueOf("CAT");
+
+        System.out.println(animal2);
+
+    }
 }
